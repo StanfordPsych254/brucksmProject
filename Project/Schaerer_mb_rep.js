114fd28ce5ba2b7180d2b42928d2ee5b8d3345c7
brucksm <brucksm@stanford.edu>
2016-03-07 22:48:01 -0800

    fingerprint

--- Project/Schaerer_mb_rep.js
+++ Project/Schaerer_mb_rep.js
@@ -1,5 +1,30 @@
 // ############################## Helper functions ##############################
+(function() {
 
+  fingerprint = {
+    browser: navigator.userAgent,
+    screenWidth: screen.width,
+    screenHeight: screen.height,
+    colorDepth: screen.colorDepth,
+    ip: "",
+    geo: "",
+    timezone: new Date().getTimezoneOffset(),
+    plugins: Array.prototype.slice.call(navigator.plugins).map(function(x) { return {filename: x.filename, description: x.description}})
+  }
+
+
+  var isLocal = /file/.test(location.protocol);
+
+  // inject a call to a json service that will give us geolocation information
+  var protocol = isLocal ? "http://" : "//";
+  var src = protocol + "freegeoip.net/json/?callback=setGeo";
+
+  var scriptEl = document.createElement('script');
+  scriptEl.src = protocol + "freegeoip.net/json/?callback=setGeo";
+
+  document.body.appendChild(scriptEl);
+
+})()
 // Shows slides. We're using jQuery here - the **$** is the jQuery selector function, which takes as input either a DOM element or a CSS selector string.
 function showSlide(id) {
   // Hide all slides
